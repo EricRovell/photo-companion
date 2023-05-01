72d5b521939580481e92c1a375b8aa7b0567f0bd
EricRovell <ericrovell@gmail.com>
2023-05-01 03:48:42 +0300

    feat: warning page

---
+++ src/lib/pages/warning/warning.page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { YEAR } from "../../constants";
+
+	const currentYear = new Date().getFullYear();
+</script>
+
+{#if currentYear !== YEAR}
+	<section id="warning">
+		<h2>
+			Предупреждение
+		</h2>
+		К сожалению, данные веб-приложения устарели.
+	</section>
+{/if}
