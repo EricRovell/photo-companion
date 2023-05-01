5da388d763293fe32d2e37a2832517503cd5d4c1
EricRovell <ericrovell@gmail.com>
2023-05-01 03:47:56 +0300

    feat: about page

---
+++ src/lib/pages/about/about.page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import styles from "./about.module.css";
+</script>
+
+<section id="about" class="{styles.root}">
+	<h2>
+		Общая информация
+	</h2>
+	<p>
+		Веб-приложение использует данные о <a target="_blank" href="https://lensvet.spb.ru/grafik_raboty_naruzhnogo_osvescheni/">графике наружного освещения в Санкт-Петербурге</a> на <a target="_blank" href="https://lensvet.spb.ru/d/26909/d/no-2023-gbu-utverzhden-1.pdf">2023 год</a>.
+	</p>
+	<p>
+		Данный график составлен с учётом фактических атмосферных условий Санкт-Петербурга.
+	</p>
+	<p>
+		Включение и отключение наружного освещения разрешается с отклонением от графика на плюс-минус 30 минут.
+	</p>
+</section>
