42834d5e371d278a9c4f4547be4336aba84f3e1f
EricRovell <ericrovell@gmail.com>
2023-05-04 14:09:26 +0300

    feat: setup pwa

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,18 @@ const app = new App({
 	target: document.getElementById("app")
 });
 
+const registerServiceWorker = async () => {
+	if ("serviceWorker" in navigator) {
+		try {
+			await navigator.serviceWorker.register("/service-worker.js", {
+				scope: "/"
+			});
+		} catch (error) {
+			console.error(`Service worker registration failed with ${error}`);
+		}
+	}
+};
+
+registerServiceWorker();
+
 export default app;
