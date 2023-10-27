1754668bad37a85a6124def52089db52084e50e1
EricRovell <ericrovell@gmail.com>
2023-10-27 23:30:17 +0300

    feat: tabs component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Bulb } from "./bulb/bulb.svelte";
 export { default as Button } from "./button/button.svelte";
 export { default as Gauge } from "./gauge/gauge.svelte";
+export { default as Tabs } from "./tabs/tabs.svelte";
 export { default as Timer } from "./timer/timer.svelte";
