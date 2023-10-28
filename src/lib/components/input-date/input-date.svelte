3ee5443154dc4fed4ddc903eeadfedab8cbae3cc
EricRovell <ericrovell@gmail.com>
2023-10-28 16:48:32 +0300

    feat: input date component

---
+++ src/lib/components/input-date/input-date.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import styles from "./input-date.module.css";
+
+	export let max: string | undefined = undefined;
+	export let min: string | undefined = undefined;
+	export let title: string | undefined = undefined;
+	export let value: string = new Date().toISOString();
+</script>
+
+<label class="{styles["input-date"]}">
+	<slot />
+	<input
+		type="date"
+		{max}
+		{min}
+		on:change
+		{title}
+		{value}
+		{...$$restProps}
+	/>
+</label>
