996fcc06d7f5fc74c3c6862ef3f5141e4f4dde54
EricRovell <ericrovell@gmail.com>
2023-10-29 01:44:44 +0300

    feat: update about tab contents

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,5 @@ export const ACTUAL_DATA = currentYear === YEAR;
 // Saint-Petersburg, Russia
 export const LAT = 59.937500;
 export const LON = 30.308611;
+
+export const title = "Наружное освещение г.Санкт-Петербурга";
