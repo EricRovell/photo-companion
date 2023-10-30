a9518c1ad798d312ab2a86217b1d6fd252aafa03
EricRovell <ericrovell@gmail.com>
2023-10-31 00:17:30 +0300

    feat: icon component

---
+++ src/lib/components/icon.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let title: string | undefined = undefined;
+	export let viewBox = "0 0 256 256";
+	export let path: string | undefined = undefined;
+</script>
+
+<svelte:options namespace="svg" />
+
+<svg
+	aria-labelledby={title ?? null}
+	role="presentation"
+	{viewBox}
+	{...$$restProps}
+>
+		{#if title}
+			<title>{title}</title>
+		{/if}
+		<path d={path} />
+</svg>
+
+<style>
+	svg {
+		aspect-ratio: 1 / 1;
+		width: var(--icon-size, 1em);
+		height: var(--icon-size, 1em);
+		fill: var(--icon-color, currentcolor);
+	}
+</style>
