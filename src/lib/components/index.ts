9ec213d6b754031aa74bfefdade7772a48b985f0
EricRovell <ericrovell@gmail.com>
2023-10-31 21:17:13 +0300

    feat: sun component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,5 +3,7 @@ export { default as Button } from "./button/button.svelte";
 export { default as Gauge } from "./gauge/gauge.svelte";
 export { default as Icon } from "./icon.svelte";
 export { default as InputDate } from "./input-date/input-date.svelte";
+export { default as Moon } from "./moon/moon.svelte";
+export { default as Sun } from "./sun/sun.svelte";
 export { default as Tabs } from "./tabs/tabs.svelte";
 export { default as Timer } from "./timer/timer.svelte";
