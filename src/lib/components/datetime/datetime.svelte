c22d22bac174d5552eab9f1d97489f7eceea729d
EricRovell <ericrovell@gmail.com>
2023-11-02 12:39:19 +0300

    feat: datetime component

---
+++ src/lib/components/datetime/datetime.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import styles from "./datetime.module.css";
+
+	export let date = new Date();
+	export let options: Intl.DateTimeFormatOptions = {};
+	export let locale = "ru-RU";
+
+	let rendered = new Intl.DateTimeFormat(locale, options).format(date);
+</script>
+
+<time
+	class="{styles.datetime}"
+	datetime="{rendered}"
+>
+	{rendered}
+</time>
