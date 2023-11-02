aa0ccbd29d0a1b208c913149996ae65ce577c9d0
EricRovell <ericrovell@gmail.com>
2023-11-02 19:24:42 +0300

    fix: validate date in helpers to provide reasonable fallback

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -10,22 +10,30 @@ export function round(number: number, digits = 0, base = Math.pow(10, digits)): 
 /**
  * Formats time from `Date` object as "HH:MM".
  */
-export const formatTime = (timestamp: number) => {
+export function formatTime(timestamp: number) {
+	if (!isValidDate(timestamp)) {
+		return "";
+	}
+
 	return Intl.DateTimeFormat("ru-RU", {
 		hour12: false,
 		hour: "numeric",
 		minute: "numeric"
 	}).format(timestamp);
-};
+}
 
 /**
  * Calculates the angle in degrees from `Date` object using time for 24 hour circle.
  */
-export const getAngleFromTime = (date = new Date()): number => {
+export function getAngleFromTime(date = new Date()): number {
+	if (!isValidDate(date)) {
+		return 0;
+	}
+
 	const hours = date.getHours();
 	const minutes = date.getMinutes();
 	return Math.round(360 * (hours * 60 + minutes) / (24 * 60));
-};
+}
 
 /**
  * Validate if the given date is within actual schedule.
@@ -37,15 +45,15 @@ export const validateDate = (date: Date) => {
 /**
  * Increments the given date by number of days.
  */
-export const incrementDateByDay = (date: Date | string, dayCount: number): Date => {
+export function incrementDateByDay(date: Date | string, dayCount: number): Date {
 	const currentDate = new Date(date);
 	const nextDate = new Date(currentDate.getTime());
 	nextDate.setDate(nextDate.getDate() + dayCount);
 
 	return nextDate;
-};
+}
 
-export const isValidDate = (date: Date | number | string | undefined): boolean => {
+export function isValidDate(date: Date | number | string | undefined): boolean {
 	if (date instanceof Date) {
 		return !isNaN(date.getTime());
 	}
@@ -55,4 +63,4 @@ export const isValidDate = (date: Date | number | string | undefined): boolean =
 	}
 
 	return false;
-};
+}
