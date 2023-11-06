1338c1c48ac28f39d8a7fa8f37ca4bc6e0061f51
EricRovell <ericrovell@gmail.com>
2023-11-06 14:00:11 +0300

    refactor: simplify code a lot

--- src/pages/sun/sun.page.svelte
+++ src/pages/sun/sun.page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { afterUpdate } from "svelte";
 	import { GaugeTime, Sun } from "@lib/components";
 	import { ViewDate } from "@lib/layout";
 	import { getSunData } from "@services/sun";
@@ -7,32 +6,28 @@
 	import styles from "./sun.module.css";
 
 	let date: string = new Date().toISOString().substring(0, 10);
-	let state: ReturnType<typeof getSunData>;
+	let state = getSunData(new Date(date), LAT, LON);
 
 	const sunSize = 57;
 
-	afterUpdate(() => {
-		state = getSunData(new Date(date), LAT, LON);
-	});
+	$: state = getSunData(new Date(date), LAT, LON);
 </script>
 
-<section id="ephemeris-sun" class="card {styles.root}">
+<section id="sun" class="card {styles.root}">
 	<header>
 		<h2>Времена восхода и захода</h2>
 	</header>
-	{#if state}
-		<ViewDate bind:date>
-			<GaugeTime
-				timeFrom="{state.sunriseStart.value}"
-				timeTo="{state.sunsetEnd.value}"
-			>
-				<Sun
-					x="-{sunSize / 2}"
-					y="-{sunSize / 2}"
-					width="{sunSize}"
-					height="{sunSize}"
-				/>
-			</GaugeTime>
-		</ViewDate>
-	{/if}
+	<ViewDate bind:date>
+		<GaugeTime
+			timeFrom="{state.sunriseStart.value}"
+			timeTo="{state.sunsetEnd.value}"
+		>
+			<Sun
+				x="-{sunSize / 2}"
+				y="-{sunSize / 2}"
+				width="{sunSize}"
+				height="{sunSize}"
+			/>
+		</GaugeTime>
+	</ViewDate>
 </section>
