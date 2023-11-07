c17013628190528f1fc26cf486eaa164027efea1
EricRovell <ericrovell@gmail.com>
2023-11-07 15:44:12 +0300

    feat: use css to clip and add precision prop

--- src/lib/components/moon/moon.svelte
+++ src/lib/components/moon/moon.svelte
@@ -6,6 +6,7 @@
 	export let rotation = 0;
 	export let size = 100;
 	export let radius = size / 2 - 5;
+	export let precision = 0.025;
 
 	let center = size / 2;
 	const phaseTweened = tweened(phase);
@@ -30,15 +31,9 @@
 	viewBox="0 0 {size} {size}"
 	style="
 		--moon-disk-angle: {getNormalizedAngleRad($phaseTweened)}rad;
-		transform: rotate({-rotation / 4}rad);
+		transform: rotate({-rotation}rad);
 	"
 >
-	<clipPath id="moon-semicircle-left">
-		<rect x="0" y="0" width="{center}" height="100"/>
-	</clipPath>
-	<clipPath id="moon-semicircle-right">
-		<rect x="{center}" y="0" width="{center}" height="100"/>
-	</clipPath>
 	<circle
 		class="{styles.shadow}"
 		cx="{center}"
@@ -47,25 +42,25 @@
 	/>
 	<circle
 		class="{styles.semicircle}"
-		class:visible="{$phaseTweened >= 0.5}"
+		class:visible="{$phaseTweened >= 0.5 && $phaseTweened < 1 - precision}"
+		data-left
 		cx="{center}"
 		cy="{center}"
 		r="{radius}"
-		clip-path="url(#moon-semicircle-left)"
 	/>
 	<circle
 		class="{styles.semicircle}"
-		class:visible="{$phaseTweened <= 0.5}"
+		class:visible="{$phaseTweened <= 0.5 && $phaseTweened >= precision}"
+		data-right
 		cx="{center}"
 		cy="{center}"
 		r="{radius}"
-		clip-path="url(#moon-semicircle-right)"
 	/>
 	<circle
-	class="{styles.diff}"
-	class:dark="{0.5 / getNormalizedPhase($phaseTweened) > 2}"
-	cx="{center}"
-	cy="{center}"
-	r="{radius}"
-/>
+		class="{styles.diff}"
+		class:dark="{0.5 / getNormalizedPhase($phaseTweened) > 2}"
+		cx="{center}"
+		cy="{center}"
+		r="{radius}"
+	/>
 </svg>
