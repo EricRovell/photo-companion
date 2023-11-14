a0bde29074e0704f0d0a31e7afe62df60cec8d5a
EricRovell <ericrovell@gmail.com>
2023-11-14 19:18:13 +0300

    feat: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import type { ComponentType } from "svelte";
+
 export type SunEventName =
 	| "dawn:astronomical"
 	| "dawn:nautical"
@@ -5,13 +7,11 @@ export type SunEventName =
 	| "dawn:civil"
 	| "dawn:blue-hour:end"
 	| "dawn:golden-hour:start"
-	| "sunrise"
 	| "sunrise:start"
 	| "sunrise:end"
 	| "dawn:golden-hour:end"
 	| "solar-noon"
 	| "dusk:golden-hour:start"
-	| "sunset"
 	| "sunset:start"
 	| "sunset:end"
 	| "dusk:golden-hour:end"
@@ -30,13 +30,55 @@ export type LightsEventName =
 	| "lights:start"
 	| "lights:end";
 
-export type EventName =
-	| LightsEventName
-	| SunEventName
-	| MoonEventName;
+export type SunColor =
+	| "astronomical"
+	| "nautical"
+	| "blue-hour"
+	| "civil"
+	| "golden-hour"
+	| "day"
+	| "night";
 
 export interface ScheduleDataItem {
 	lights: boolean;
 	event: LightsEventName;
 	timestamp: number
+}
+
+export type EventName =
+	| LightsEventName
+	| SunEventName
+	| MoonEventName;
+
+export interface Event<Name extends string, Data = Partial<Record<string, never>>> {
+	name: Name;
+	timestamp: number;
+	data: Data;
+}
+
+export type LightsEvent = Event<LightsEventName>;
+
+export type MoonEvent = Event<MoonEventName, {
+	phase: number,
+	waxing: boolean,
+	fraction: number
+}>;
+
+export type SunEvent = Event<SunEventName, {
+	azimuth: number,
+	elevation: number;
+}>;
+
+export type TimelineEvent = LightsEvent | MoonEvent | SunEvent;
+
+export interface TimelineEntry {
+	date: Date;
+	items: TimelineEvent[];
+}
+
+export interface EventComponent<Props> {
+	component: ComponentType;
+	props: Props;
+	message: string;
+	title: string;
 }
