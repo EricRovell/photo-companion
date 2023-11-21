1dbd690e0cf9ff5a3764fce9e95e0836436bc955
EricRovell <ericrovell@gmail.com>
2023-11-21 23:57:22 +0300

    chore: setup markdown preprocessor

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,15 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+/**
+ * Taking from <reference types="mdsvex/globals" />
+ */
+declare module "*.mdx" {
+	import type { SvelteComponent } from "svelte";
+
+	export default class Comp extends SvelteComponent {
+		$$prop_def: object;
+	}
+	export const metadata: Record<string, unknown>;
+}
