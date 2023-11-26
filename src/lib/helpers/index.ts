31be90cbedd5ef75e39b9bab1373a73cdad694ff
EricRovell <ericrovell@gmail.com>
2023-11-26 14:45:32 +0300

    refactor: with date layout

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -145,3 +145,33 @@ export const secondsToHoursAndMinutes = (seconds: number): [ hours: number, minu
 
 	return [ hours, minutes ];
 };
+
+
+export function parseQueryDate(input: string): string {
+	if (!input) {
+		return getDateTimeString();
+	}
+
+	const [ yy, mm, dd, h, m ] = input.split("-").map(Number);
+
+	const date = new Date(yy, mm - 1, dd, h, m);
+
+	if (!isValidDate(date)) {
+		return "invalid";
+	}
+
+	return getDateTimeString(date);
+}
+
+export function createQueryDate(d: Date = new Date()): string {
+	return [
+		d.getFullYear(),
+		d.getMonth() + 1,
+		d.getDate(),
+		d.getHours(),
+		d.getMinutes()
+	]
+		.map(value => value < 10 ? `0${value}` : value.toString())
+		.join("-");
+}
+
--- src/lib/layout/with-date/with-date.svelte
+++ src/lib/layout/with-date/with-date.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
-	import { fade } from "svelte/transition";
 	import { query } from "svelte-pathfinder";
-	import { Button, InputDatetime } from "@lib/components";
-	import { getDateTimeString, isValidDate } from "@lib/helpers";
-	import { parseQueryDate, createQueryDate } from "./with-date.helpers";
-	import { dict } from "@lib/dict";
-	import type { ComponentType } from "svelte";
+	import ErrorDate from "@lib/../pages/error-date/error-date.svelte";
+	import { InputDatetime } from "@lib/components";
+	import { isValidDate, parseQueryDate, createQueryDate } from "@lib/helpers";
+	import { onMount, type ComponentType } from "svelte";
 	import styles from "./with-date.module.css";
 
 	export let page: ComponentType;
 
 	let value = parseQueryDate($query.date as string);
 	let date = new Date(value);
-
-	const handleClick = () => {
-		const date = new Date();
-		value = getDateTimeString(date);
-		$query.date = createQueryDate(date);
-	};
 
 	const handleChange = (event: CustomEvent<{ value: string }>) => {
 		date = new Date(event.detail.value);
@@ -32,23 +24,23 @@
 		value = parseQueryDate($updated.date as string);
 		date = new Date(value);
 	});
+
+	onMount(() => {
+		if (!$query.date) {
+			$query.date = createQueryDate();
+		}
+	});
 </script>
 
-{#if isValidDate(date)}
-	<svelte:component this="{page}" {date} />
+{#if !isValidDate(date)}
+	<ErrorDate />
 {:else}
-	<aside class="card {styles["error"]}" in:fade>
-		<header>{dict["error"]}</header>
-		<p>{dict["date-error-desc"]}:</p>
-		<ul>
-			<li>{dict["date-error-check-url"]} <strong>YYYY-MM-DD-hh-mm;</strong></li>
-			<li>{dict["date-error-check-input"]};</li>
-		</ul>
-		<p>{dict["date-error-set-current-desc"]}:</p>
-		<Button on:click="{handleClick}">
-			{dict["date-error-set-current"]}
-		</Button>
-	</aside>
+	<svelte:component
+		{date}
+		lat="{Number($query.lat)}"
+		lon="{Number($query.lon)}"
+		this="{page}"
+	/>
 {/if}
 
 <InputDatetime
