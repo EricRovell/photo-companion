c7253d6ac18fe8ca70b5ecbd585d2583b1568300
EricRovell <ericrovell@gmail.com>
2024-03-20 12:24:56 +0300

    chore(docs): photo-companion

--- src/apps/photo-companion/src/lib/routes.ts
+++ src/apps/photo-companion/src/lib/routes.ts
@@ -1,8 +1,6 @@
 export { homepage as urlGithub } from "../../package.json";
 export const urlLightsScheduleSourceSPb = "https://lensvet.spb.ru/grafik_raboty_naruzhnogo_osvescheni/";
 export const urlLightsScheduleSourceMoscow = "https://domdata.ru/osveschenie-v-moskve";
-export const urlPWA = "https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B5%D1%81%D1%81%D0%B8%D0%B2%D0%BD%D0%BE%D0%B5_%D0%B2%D0%B5%D0%B1-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5";
-export const urlSuncalcNpm = "https://www.npmjs.com/package/suncalc3";
 export const urlCommitHash = "https://github.com/ericrovell/photo-companion/commit/__COMMIT_HASH__";
 
 export const routeBridges = "/bridges";
