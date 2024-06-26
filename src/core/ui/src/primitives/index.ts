d98c34351037fd0b9599020812413e44fb19802d
EricRovell <ericrovell@gmail.com>
2024-06-26 17:02:14 +0300

    feat: document visibility reactive primitive

--- src/core/ui/src/primitives/index.ts
+++ src/core/ui/src/primitives/index.ts
@@ -3,4 +3,5 @@ export { createResizeObserver } from "./create-resize-observer";
 export { createTimeoutLoop, makeTimer } from "./timer";
 export { createToggle } from "./toggle";
 export { createTweened, type TweenedProps } from "./tweened";
+export { useDocumentVisibility } from "./use-document-visibility";
 export { useSwipe } from "./use-swipe";
