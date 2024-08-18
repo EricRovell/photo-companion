1fe309c0fc40e488f18365d4d633625faaf5efd9
EricRovell <ericrovell@gmail.com>
2024-08-18 14:00:43 +0300

    feat: unit types

---
+++ src/packages/meeus-algorithms/src/types/units.ts
@@ -0,0 +1,87 @@
+/**
+ * Angular degree
+ */
+export type Degree = number;
+
+/**
+ * Angular radian
+ */
+export type Radian = number;
+
+/**
+ * Terrestrial year (355 or 366 days)
+ */
+export type Year = number;
+
+/**
+ * Terrestrial month
+ */
+export type Month = number;
+
+/**
+ * Terrestrial day (24 hours)
+ */
+export type Day = number;
+
+/**
+ * Time hour
+ */
+export type Hour = number;
+
+/**
+ * Time minute
+ */
+export type Minute = number;
+
+/**
+ * Time second
+ */
+export type Second = number;
+
+/**
+ * Time millisecond
+ */
+export type Millisecond = number;
+
+/**
+ * Julian day
+ * 
+ * The Julian Day is a continuous count of days
+ * and fractions thereof from the beginning of the
+ * year -4712.
+ * 
+ * By tradition, the Julian Day begins at Greenwich noon,
+ * that is, 12h Universal Time.
+ */
+export type JulianDay = number;
+
+/**
+ * Julian century (time interval of 36 525 days)
+ */
+export type JulianCentury = number;
+
+/**
+ * Julian Millennium (time interval of 365 250 days)
+ */
+export type JulianMillennium = number;
+
+/**
+ * Mean radius of the Sun
+ */
+export type SolarRadius = number;
+
+/**
+ * Astronomical Unit (AU), that is mean distance
+ * between the center of the Sun and the Earth.
+ */
+export type AstronomicalUnit = number;
+
+/**
+ * Meter length
+ */
+export type Meter = number;
+
+/**
+ * Kilometer length
+ */
+export type Kilometer = number;
