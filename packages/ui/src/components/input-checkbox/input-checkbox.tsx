967fb375fd589e1920a12e128bf997823a2a27d2
EricRovell <ericrovell@gmail.com>
2025-02-15 20:22:27 +0300

    refactor(ui): checkbox

--- packages/ui/src/components/input-checkbox/input-checkbox.tsx
+++ packages/ui/src/components/input-checkbox/input-checkbox.tsx
@@ -1,11 +1,38 @@
 import { createEffect, mergeProps, Show, splitProps  } from "solid-js";
 import { classnames } from "utils";
 
-import { DEFAULT_PROPS, ICON_PATH } from "./input-checkbox.consts";
+import type { JSX } from "solid-js";
 
-import type { CheckboxIconProps, InputCheckboxProps } from "./input-checkbox.types";
+import type { Classes } from "../../types";
 
 import styles from "./input-checkbox.module.css";
+
+type SymbolType = Nullish<"CHECK" | "CROSS" | "MINUS">;
+
+interface CheckboxIconProps {
+	class?: string;
+	symbolChecked?: SymbolType;
+	symbolIndeterminate?: SymbolType;
+	symbolUnchecked?: SymbolType;
+}
+
+interface InputCheckboxProps extends Omit<JSX.InputHTMLAttributes<HTMLInputElement>, "class">, CheckboxIconProps {
+	classes?: Classes<"icon" | "input" | "label" | "title">;
+	indeterminate?: Nullable<boolean>;
+	label?: string;
+	ref?: HTMLInputElement;
+}
+
+const DEFAULT_PROPS: InputCheckboxProps = {
+	symbolChecked: "CHECK",
+	symbolIndeterminate: "MINUS"
+};
+
+const ICON_PATH = {
+	CHECK : "M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z",
+	CROSS: "M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z",
+	MINUS: "M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128Z"
+};
 
 function CheckboxIcon(props: CheckboxIconProps) {
 	return (
