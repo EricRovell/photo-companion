08229e923b18d82d1446533d61d50c70f37de0f9
EricRovell <ericrovell@gmail.com>
2025-03-08 16:26:03 +0300

    refactor: use context creator utility

--- packages/photo-companion/src/services/geolocation/geolocation.service.tsx
+++ packages/photo-companion/src/services/geolocation/geolocation.service.tsx
@@ -1,28 +1,71 @@
-import { createContext, useContext } from "solid-js";
+import { createContext } from "solid-js";
+import { onMount } from "solid-js";
+import { createStore } from "solid-js/store";
 import { isNullable } from "utils/validators";
 
-import type { ParentProps} from "solid-js";
+import { createProvider } from "~/helpers/create-provider";
 
-import { createGeolocationState } from "./geolocation.state";
+interface GeolocationStore {
+	coordinates: GeolocationCoordinates | null;
+	error: GeolocationPositionError | null;
+	status: "error" | "fulfilled" | "pending";
+	supports: boolean;
+}
+
+const INITIAL_STATE: GeolocationStore = {
+	coordinates: null,
+	error: null,
+	status: "fulfilled",
+	supports: false
+};
+
+function createGeolocationState() {
+	const [ store, setStore ] = createStore<GeolocationStore>(INITIAL_STATE);
+
+	onMount(() => {
+		if (!isNullable(navigator.geolocation)) {
+			setStore("supports", true);
+		}
+	});
+
+	function getGeolocation(onSuccess?: PositionCallback, onError?: PositionErrorCallback) {
+		setStore({
+			error: null,
+			status: "pending"
+		});
+
+		function handleSuccess(position: GeolocationPosition) {
+			setStore({
+				coordinates: position.coords,
+				status: "fulfilled"
+			});
+
+			onSuccess?.(position);
+		}
+
+		function handleError(error: GeolocationPositionError) {
+			setStore({
+				error: error,
+				status: "error"
+			});
+
+			onError?.(error);
+		}
+
+		navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+	}
+
+	return {
+		geolocation: store,
+		getGeolocation
+	};
+}
 
 const GeolocationServiceContext = createContext<ReturnType<typeof createGeolocationState>>();
 
-export function GeolocationProvider(props: ParentProps) {
-	const state = createGeolocationState();
-
-	return (
-		<GeolocationServiceContext.Provider value={state}>
-			{props.children}
-		</GeolocationServiceContext.Provider>
-	);
-}
-
-export function useGeolocationService() {
-	const value = useContext(GeolocationServiceContext);
-
-	if (isNullable(value)) {
-		throw new Error("useGeolocationService must be used with a GeolocationServiceContext.Provider");
-	}
-
-	return value;
-}
+export const [ GeolocationProvider, useGeolocationService ] = createProvider({
+	consumerName: "useGeolocationService",
+	Context: GeolocationServiceContext,
+	getValue: createGeolocationState,
+	providerName: "Geolocation"
+});
