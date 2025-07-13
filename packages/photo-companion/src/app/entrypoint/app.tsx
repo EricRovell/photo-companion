0fd7ffa6c28643a86a44d66a490f81fd29444bd1
EricRovell <ericrovell@gmail.com>
2025-07-13 19:49:11 +0300

    refactor: move entrypoint

---
+++ packages/photo-companion/src/app/entrypoint/app.tsx
@@ -0,0 +1,33 @@
+import { MetaProvider } from "@solidjs/meta";
+import { Suspense } from "solid-js";
+import { Toaster } from "solid-sonner";
+
+import { ToastSuggestUpdate } from "~/components";
+import { Routes } from "~/routes";
+import { NavigationServiceProvider } from "~/services/navigation";
+import { ServiceWorkerProvider } from "~/services/service-worker";
+import { SettingsProvider } from "~/services/settings";
+import { TranslationProvider } from "~/services/translation";
+
+import "../styles/globals.css";
+import "../styles/main.css";
+import "../styles/utils.css";
+import "ui/styles/tokens.css";
+
+export const App = () => (
+	<ServiceWorkerProvider>
+		<MetaProvider>
+			<SettingsProvider>
+				<NavigationServiceProvider>
+					<Suspense>
+						<TranslationProvider>
+							<Routes />
+							<ToastSuggestUpdate />
+							<Toaster position="bottom-center" richColors theme="dark" />
+						</TranslationProvider>
+					</Suspense>
+				</NavigationServiceProvider>
+			</SettingsProvider>
+		</MetaProvider>
+	</ServiceWorkerProvider>
+);
